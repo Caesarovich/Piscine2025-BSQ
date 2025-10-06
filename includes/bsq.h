c182f7619511d44eebb384e731fb55e99e431167
ansaccar <ansaccar@student.42lyon.fr>
2025-10-06 19:47:04 +0200

    Add map struct

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,11 +6,21 @@
 /*   By: ansaccar <ansaccar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:52:58 by ansaccar          #+#    #+#             */
-/*   Updated: 2025/10/06 18:53:21 by ansaccar         ###   ########.fr       */
+/*   Updated: 2025/10/06 19:37:44 by ansaccar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BSQ_H
 # define BSQ_H
+
+typedef struct s_map
+{
+	char	empty;
+	char	obsticle;
+	char	fill;
+	int		columns_count;
+	int		lines_count;
+	char	**grid;
+}	t_map;
 
 #endif
