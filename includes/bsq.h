b44fdc95a33038ba59e3d4290fd62d44fd2515a5
ansaccar <ansaccar@student.42lyon.fr>
2025-10-07 17:11:37 +0200

    Add strlcpy

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,7 +6,11 @@
 /*   By: alaurent <alaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:52:58 by ansaccar          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/10/07 14:33:42 by alaurent         ###   ########.fr       */
+=======
+/*   Updated: 2025/10/07 17:10:46 by ansaccar         ###   ########.fr       */
+>>>>>>> 6bac50c (Add strlcpy)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +34,22 @@ typedef struct s_map
 	unsigned int	**dp;
 }	t_map;
 
-t_map	*create_map(unsigned int w, unsigned int h);
-void	map_print(t_map *map);
-t_map	*parse_file(char *path);
+t_map			*create_map(unsigned int w, unsigned int h);
+void			map_print(t_map *map);
+t_map			*parse_file(char *path);
 
-void	ft_putchar(char c);
-void	solver(t_map *map);
-int		ft_strlen(char *str);
-void	ft_putstr(char *str);
-void	ft_putnbr(int nbr);
-int		ft_strcmp(char *s1, char *s2);
-int		ft_char_is_num(char c);
-int		ft_char_is_ws(char c);
-int		ft_char_is_printable(char c);
-int		ft_atoi(char *str);
-char	*read_whole_file(char *path);
-void	ft_memcpy(void *dest, void *src, size_t size);
-void	ft_bzero(void *dest, size_t size);
+void			ft_putchar(char c);
+int				ft_strlen(char *str);
+void			ft_putstr(char *str);
+void			ft_putnbr(int nbr);
+int				ft_strcmp(char *s1, char *s2);
+int				ft_char_is_num(char c);
+int				ft_char_is_ws(char c);
+int				ft_char_is_printable(char c);
+int				ft_atoi(char *str);
+char			*read_whole_file(char *path);
+void			ft_memcpy(void *dest, void *src, size_t size);
+void			ft_bzero(void *dest, size_t size);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 
 #endif
---
+++ srcs/ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansaccar <ansaccar@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 14:36:59 by ansaccar          #+#    #+#             */
+/*   Updated: 2025/10/07 14:37:12 by ansaccar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (src[i] && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
